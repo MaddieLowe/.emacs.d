2cf62b89a7db79a7a6bd499cf1cdf8d311a49cf7
Maddie Lowe <maddie.lowe27@gmail.com>
2016-09-08 15:58:53 -0600

    Incorporated some of Torin's emacs styles into init file

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-(require 'cask "~/src/cask/cask.el")
+(require 'cask "~/.cask/cask.el")
 (cask-initialize)
 
 (require 'pallet)
@@ -9,17 +9,22 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
- '(custom-enabled-themes (quote (manoj-dark))))
+;; '(ansi-color-faces-vector
+;;   [default default default italic underline success warning error])
+;; '(ansi-color-names-vector
+;;   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
+;; '(custom-enabled-themes (quote (manoj-dark))))
+ '(indent-tabs-mode nil))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "#212121" :foreground "grey" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 83 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
+ '(shell-option-face ((t (:foreground "red"))) t)
+ '(shell-output-face ((t (:foreground "blue" :italic nil))) t)
+ '(shell-prompt-face ((t (:foreground "blue" :bold t))) t))
 
 (cua-mode t)
 (tool-bar-mode -1)
@@ -35,4 +40,8 @@
 (global-set-key [M-up] 'windmove-up)
 (global-set-key [M-down] 'windmove-down)
 
-(set-face-attribute 'default nil :height 100)
+;;(set-face-attribute 'default nil :height 100)
+
+(set-background-color "black")
+(set-foreground-color "grey")
+(set-cursor-color "grey")
