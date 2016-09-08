df21ba71bdd6ab98c74016bf2060e94921736df8
Maddie Lowe <maddie.lowe27@gmail.com>
2016-09-08 13:45:16 -0600

    Created new emacs repo

---
+++ init.el
@@ -0,0 +1,38 @@
+(require 'cask "~/src/cask/cask.el")
+(cask-initialize)
+
+(require 'pallet)
+(pallet-mode t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
+ '(custom-enabled-themes (quote (manoj-dark))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(cua-mode t)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(font-lock-mode t)
+(global-linum-mode t)
+(setq-default show-trailing-whitespace t)
+(show-paren-mode t)
+(setq-default truncate-lines t)
+
+(global-set-key [M-left] 'windmove-left)
+(global-set-key [M-right] 'windmove-right)
+(global-set-key [M-up] 'windmove-up)
+(global-set-key [M-down] 'windmove-down)
+
+(set-face-attribute 'default nil :height 100)
